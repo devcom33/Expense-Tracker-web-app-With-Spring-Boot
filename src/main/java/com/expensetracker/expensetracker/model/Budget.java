fde89b5d9a4c1c85eaf4fa75e4e48226bb5bb458
devcom33 <mouad.elb06@gmail.com>
2023-12-18 21:33:11 +0100

    add new model Budget

--- src/main/java/com/expensetracker/expensetracker/model/Budget.java
+++ src/main/java/com/expensetracker/expensetracker/model/Budget.java
@@ -1,5 +1,29 @@
 package com.expensetracker.expensetracker.model;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Budget {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int budget_id;
+    private double budget_limit;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Category category;
 }
